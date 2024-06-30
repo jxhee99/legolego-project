f50086063aa13e4a8f52fdd3ec2c8bfebde93f8b
“jxhee99” <wngml2666@gmail.com>
2024-06-30 22:00:26 +0900

    fix/ wishlist - wishlist 찜하기/찜 취소 수정

--- legolego/src/main/java/com/kosta/legolego/products/service/WishlistService.java
+++ legolego/src/main/java/com/kosta/legolego/products/service/WishlistService.java
@@ -38,16 +38,27 @@ public class WishlistService {
         // 2. 사용자 중복 찜 불가능(패키지 하나 - 유저 한 명)
         Optional<Wishlist> existingWish = wishlistRepository.findByUser_userNumAndProduct_productNum(userNum, productNum);
         if(existingWish.isPresent()) {
-            throw new RuntimeException("이미 찜한 상품입니다.");
+            // 이미 존재하는 찜 항목이 있는 경우 상태 true 업데이트
+            Wishlist wishlist = existingWish.get();
+
+            if(wishlist.isWishlistStatus()) {
+                // 찜 상태 true 일 때
+                throw new RuntimeException("이미 찜한 상품입니다.");
+
+            } else {
+                // 찜 상태 false 일 때
+                wishlist.setWishlistStatus(true);
+                wishlistRepository.save(wishlist);
+            }
+
+        } else {
+            // 3. 존재하지 않는 경우 새로운 찜 항목 생성
+            Wishlist newWishlist = new Wishlist();
+            newWishlist.setUser(user);
+            newWishlist.setProduct(product);
+            newWishlist.setWishlistStatus(true);
+            wishlistRepository.save(newWishlist);
         }
-
-        // 3. 찜하기 엔티티 생성 후 저장
-        Wishlist wishlist = new Wishlist();
-        wishlist.setUser(user);
-        wishlist.setProduct(product);
-        wishlist.setWishlistStatus(true);
-        wishlistRepository.save(wishlist);
-
         // 4. 상품 테이블의 wishlist count 필드 업데이트
         product.setWishlistCount(product.getWishlistCount() +1);
         productRepository.save(product);
@@ -66,7 +77,6 @@ public class WishlistService {
     public void  removeFromWishlist(Long userNum, Long productNum){
         Wishlist wishlist = wishlistRepository.findByUser_userNumAndProduct_productNum(userNum, productNum)
                 .orElseThrow(() -> new ResolutionException("찜 목록에서 상품을 찾을 수 없습니다."));
-        wishlistRepository.delete(wishlist);
         wishlist.setWishlistStatus(false);
         wishlistRepository.save(wishlist);
 
