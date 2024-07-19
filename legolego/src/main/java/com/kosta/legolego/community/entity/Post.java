3f82f2c5ace447fd597d6b2c2e4c7555cf64a61a
“jxhee99” <wngml2666@gmail.com>
2024-07-19 13:52:06 +0900

    feat : 지난여행 글자수 수정, local db 정보 추가

--- legolego/src/main/java/com/kosta/legolego/community/entity/Post.java
+++ legolego/src/main/java/com/kosta/legolego/community/entity/Post.java
@@ -35,7 +35,7 @@ public class Post {
     @Column(name = "title", nullable = false)
     private String title;
 
-    @Column(name = "content", nullable = false)
+    @Column(name = "content", nullable = false, length = 1000)
     private String content;
 
     @ManyToOne
